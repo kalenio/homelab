41887c93cf632c02bd930d13813efc4f5b868bb7
kalen <123925547+kalenio@users.noreply.github.com>
2025-08-02 12:46:11 -0600

    Update monitor_nic_fail.sh

--- monitor_nic_fail.sh
+++ monitor_nic_fail.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create: nano /usr/local/bin/monitor_nic_fail.sh
-# Add this line to crontab (crontab -e) to run on reboot: @reboot /path/to/monitor_nic_fail.sh
+# Add this line to crontab (crontab -e) to run on reboot: @reboot /usr/local/bin/monitor_nic_fail.sh
 
 # Define the NIC name and the log to monitor
 NIC="eno2"
